3c25c1ca0035c02b60900e5d382d21f65cf5e153
DGSWDongHyun <ydh76767@naver.com>
2020-06-30 12:07:16 +0900

    [ADDED] from now, you can remove list item when swipe to right.

--- CASH/app/src/main/java/com/dgsw_dev/cash/view/activity/MainActivity.java
+++ CASH/app/src/main/java/com/dgsw_dev/cash/view/activity/MainActivity.java
@@ -22,6 +22,7 @@ import com.dgsw_dev.cash.DataBinderMapperImpl;
 import com.dgsw_dev.cash.R;
 import com.dgsw_dev.cash.data.DataSubject;
 import com.dgsw_dev.cash.view.service.Service_Overlay;
+import com.dgsw_dev.cash.view.util.SwipeDismissListViewTouchListener;
 import com.dgsw_dev.cash.view.view_holder.ViewHolder_Data;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -33,7 +34,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 42;
-    ListView listview ;
+    ListView listview;
     ArrayList<DataSubject> list;
     SwipeRefreshLayout swipe;
     ViewHolder_Data adapter;
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
-        adapter = new ViewHolder_Data() ;
+        adapter = new ViewHolder_Data();
         list = loadSharedPreferencesList(getApplicationContext());
 
         // 리스트뷰 참조 및 Adapter달기
@@ -62,10 +63,37 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
         returnTimes(date, simpleDateFormat);
 
-        for(int index = 0; index < list.size(); index++){
+        for (int index = 0; index < list.size(); index++) {
             adapter.addItem(list.get(index).getSubjectName(), list.get(index).getToTime(), list.get(index).getDetail_time(), list.get(index).getOverTime());
             adapter.notifyDataSetChanged();
         }
+
+
+        SwipeDismissListViewTouchListener touchListener =
+                new SwipeDismissListViewTouchListener(listview,
+                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    list.remove(position);
+                                }
+                                saveSharedPreferencesList(getApplicationContext(),list);
+                                list = loadSharedPreferencesList(getApplicationContext());
+                                adapter.clearAll();
+                                for (int index = 0; index < list.size(); index++) {
+                                    adapter.addItem(list.get(index).getSubjectName(), list.get(index).getToTime(), list.get(index).getDetail_time(), list.get(index).getOverTime());
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        });
+        listview.setOnTouchListener(touchListener);
+        listview.setOnScrollListener(touchListener.makeScrollListener());
+
     }
     public void checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
@@ -125,7 +153,14 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             }
         }, 200);
     }
-
+    public static void saveSharedPreferencesList(Context context, ArrayList<DataSubject> Subject) {
+        SharedPreferences mPrefs = context.getSharedPreferences("pref",context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(Subject);
+        prefsEditor.putString("Subject_Data", json);
+        prefsEditor.commit();
+    }
     public static ArrayList<DataSubject> loadSharedPreferencesList(Context context) {
         ArrayList<DataSubject> data = new ArrayList<DataSubject>();
         SharedPreferences mPrefs = context.getSharedPreferences("pref", context.MODE_PRIVATE);
